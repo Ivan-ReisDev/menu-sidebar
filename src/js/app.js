c539a5976c6b98b0ae936f7c80c14e57179a3323
Ivan Reis <109477902+Ivan-ReisDev@users.noreply.github.com>
2023-05-13 10:29:36 -0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,20 @@
 // Adiciona a classe 'active' ao link atualmente selecionado
 var $btnOpen = document.querySelector("#btn-open");
 var $navList = document.querySelector('#nav-list');
+var $icon = document.getElementById('icon');
 var sidebar = false;
 
 function btn(){
     sidebar = !sidebar;
     if(sidebar) {
-        $navList.style.transform = "translate(0%)"
+        $navList.style.transform = "translate(0%)";
+        $icon.classList.toggle("fa-bars")
+        $icon.classList.toggle("fa-xmark")
     } else {
         $navList.style.transform = ""
+        $icon.classList.toggle("fa-xmark")
+        $icon.classList.toggle("fa-bars")
+        
     }
 }
 
